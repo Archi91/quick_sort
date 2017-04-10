a35f585a8da6b6b256327b6ed68a8359eff77b05
Archi91 <olshovskiy@gmail.com>
2017-04-10 14:39:32 +0200

    final

--- quick_sort.py
+++ quick_sort.py
@@ -1,25 +1,36 @@
 import sys
+import time
 
 class Data:
 
     def get_data(filename):
-        lines = [line.rstrip('\n') for line in open(filename, 'r')]
+        lines = [int(line.rstrip('\n')) for line in open(filename, 'r')]
         return lines
 
-lista = Data.get_data(str(sys.argv[1]))
 
 
 class QuickSort:
 
     @staticmethod
     def sort_collection(collection):
-        pivot = collection[len(collection)//2]
-        first_element = collection[0]
-        last_element = collection[len(collection)-1]
+        less = []
+        equal = []
+        greater = []
+        if len(collection) > 1:
+            pivot = collection[0]
+            for x in collection:
+                if x < pivot:
+                    less.append(x)
+                if x == pivot:
+                    equal.append(x)
+                if x > pivot:
+                    greater.append(x)
+            return(QuickSort.sort_collection(less) + equal + QuickSort.sort_collection(greater))
+        else:
+            return collection
 
-        # for element in range(collection[]):
-        #     pass
-        print(pivot, first_element, last_element)
-
-
-QuickSort.sort_collection(lista)
+start_time = time.time()
+print(QuickSort.sort_collection(Data.get_data(str(sys.argv[1]))))
+end_time = time.time()
+final = start_time - end_time
+print(final)
